9613f15d530911bcb98a269ec20395d5ddc1f9e8
bravesailor <foreverxuliming@gmail.com>
2025-03-21 16:31:25 +0800

    ---update to the newest code

--- run_mv_gradio.sh
+++ run_mv_gradio.sh
@@ -7,4 +7,4 @@ host='0.0.0.0'
 queue_size=10
 concurrency=1
 # python gradio_app_mv_ff.py --model_path '/data/models/Hunyuan3D-2' --subfolder "hunyuan3d-dit-v2-0"  --port 8080 --device 'cuda:0'
-python gradio_app_mv_ff.py --queue_size ${queue_size} --concurrency ${concurrency} --model_path ${model_path} --subfolder ${sub_folder}  --port ${port} --host ${host} --device ${device} 
+python gradio_app_feature.py --queue_size ${queue_size} --concurrency ${concurrency} --texgen_model_path ${texgen_model_path} --model_path ${model_path} --subfolder ${sub_folder}  --port ${port} --host ${host} --device ${device} 
